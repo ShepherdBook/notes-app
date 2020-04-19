8a984019cea357bfbac8e2e9ee1f9e1ba998f3f8
Zach Johnston <zachjohnston26@pm.me>
2020-04-19 16:42:21 -0400

    feat(NoteCreate): Local preview of new note

--- src/NoteCreate.js
+++ src/NoteCreate.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NoteCreate() {
+
+    const [ value, setValue ] = useState('')
+
     return(
-        <h1>New Note</h1>
+        <div>
+            <h1>New Note</h1>
+            <div>
+                <input type='text' 
+                       value={value}
+                       onChange={e => setValue(e.target.value)}/>
+            </div>
+            <div>
+                <h3>Preview</h3>
+                <span>{value}</span>
+            </div>
+        </div>
     )
 }
 
